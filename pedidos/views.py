18036116861c760eb1ea2f37ffb297edc286f125
Fabio Urbina <fabiourbinasoares@gmail.com>
2026-07-02 15:19:18 -0300

    Restaura logout_view

--- pedidos/views.py
+++ pedidos/views.py
@@ -73,3 +73,8 @@ def login_view(request):
         messages.error(request, "Usuário ou senha inválidos.")
 
     return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/login/')
