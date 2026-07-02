862ca66fe5dc3156711b9d39c3fd7cd2d7815e1c
Fabio Urbina <fabiourbinasoares@gmail.com>
2026-07-02 14:27:38 -0300

    Configura ALLOWED_HOSTS para Railway

--- portal/settings.py
+++ portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@m5cwo+345q66%zgay+ns3o-%z^1a)ixuaandreyx(n6oxm=yb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
