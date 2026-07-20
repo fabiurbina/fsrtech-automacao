6325efb9dc85dba9f8d65e2c1632fb0bac8158a9
Fabio Urbina <fabiourbinasoares@gmail.com>
2026-07-20 10:11:16 -0300

    Inclusão do alterar senha

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -1,8 +1,25 @@
 from django.urls import path
-from .views import home, login_view, logout_view
+from .views import home, login_view, logout_view    
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name='home'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path(
+        "alterar-senha/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="alterar_senha.html"
+        ),
+        name="password_change",
+    ),
+
+    path(
+        "alterar-senha/concluido/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="alterar_senha_sucesso.html"
+        ),
+        name="password_change_done",
+    ),
+    
 ]
