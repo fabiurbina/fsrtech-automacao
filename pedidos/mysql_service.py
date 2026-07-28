4835eb37c8d590b405231e5bec05628ec3ee3374
Fabio Urbina <fabiourbinasoares@gmail.com>
2026-07-28 13:43:59 -0300

    Mylhoria do sql

--- pedidos/mysql_service.py
+++ pedidos/mysql_service.py
@@ -86,20 +86,22 @@ def consultar_todos_pedidos():
     SELECT
     
             cl.razao_social,
-			p.numero_pedido,
-			p.etapa,
-			e.descricao_etapa,
-			p.codigo_cliente,
-			data_previsao,
-			REGEXP_REPLACE(c.cnpj_cpf, '[^0-9]', '') as cnpj_cpf,
-			op.numero_op,
-            pro.descricao as descricao_produto,
+            p.numero_pedido,
+            p.etapa,
+            e.descricao_etapa,
+            p.codigo_cliente,
+            data_previsao,
+            REGEXP_REPLACE(c.cnpj_cpf, '[^0-9]', '') as cnpj_cpf,
+            op.numero_op,
+            case  when pro.descricao is null then it.descricao else pro.descricao
+            end
+            as descricao_produto,
             op.numero_CodProduto,	
-            op.quantidade,
-			op.etapaid,
-            op.numero_CodOP,
-			case when eop.descricao_etapa is null and rc.dtSugestao is not null then 
-			'Requisição de compra' else eop.descricao_etapa end as 'status_producao'
+            case when op.quantidade is null then it.quantidade else op.quantidade
+            end as quantidade,
+            op.etapaid,
+            case when eop.descricao_etapa is null and rc.dtSugestao is not null then 
+            'Requisição de compra' else eop.descricao_etapa end as 'status_producao'
 			
 		FROM pedidos p
 		LEFT JOIN (SELECT * FROM etapas WHERE descricao_operacao = 'Venda de Produto') e ON e.codigo_etapa = p.etapa
@@ -109,6 +111,7 @@ def consultar_todos_pedidos():
 		LEFT JOIN (SELECT * FROM etapas WHERE descricao_operacao = 'Ordem de Produção') eop ON eop.codigo_etapa = op.etapaid
         LEFT JOIN produtos pro on pro.codigo_produto = op.numero_CodProduto
         LEFT JOIN clientes cl on cl.codigo_cliente_omie = p.codigo_cliente
+        LEFT JOIN itens_pedido it on it.numero_pedido = p.numero_pedido
 		WHERE
         (
             eop.descricao_etapa IS NOT NULL
