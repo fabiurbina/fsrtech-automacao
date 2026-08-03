5283648f5e62f49ae6574a4471b66ca7d30edf8c
Fabio Urbina <fabiourbinasoares@gmail.com>
2026-08-03 09:23:56 -0300

    Atualização do MYSQL para consultar o pedido

--- pedidos/mysql_service.py
+++ pedidos/mysql_service.py
@@ -22,7 +22,6 @@ def consultar_pedidos_cliente(cpf_cnpj):
     		
     
     SELECT
-    
             cl.razao_social,
 			p.numero_pedido,
 			p.etapa,
@@ -39,7 +38,8 @@ def consultar_pedidos_cliente(cpf_cnpj):
             end as quantidade,
 			op.etapaid,
 			case when eop.descricao_etapa is null and rc.dtSugestao is not null then 
-			'Requisição de compra' else eop.descricao_etapa end as 'status_producao'
+			'Requisição de compra' else eop.descricao_etapa end as 'status_producao',
+            it.codigo_produto
 			
 		FROM pedidos p
 		LEFT JOIN (SELECT * FROM etapas WHERE descricao_operacao = 'Venda de Produto') e ON e.codigo_etapa = p.etapa
@@ -56,6 +56,7 @@ def consultar_pedidos_cliente(cpf_cnpj):
             OR rc.dtSugestao IS NOT NULL
         )
         AND REGEXP_REPLACE(c.cnpj_cpf, '[^0-9]', '') = %s
+        group by p.numero_pedido,  it.codigo_produto
         ORDER BY p.data_previsao DESC;
     """
 
@@ -84,40 +85,42 @@ def consultar_todos_pedidos():
     		
     
     SELECT
-    
-            cl.razao_social,
-            p.numero_pedido,
-            p.etapa,
-            e.descricao_etapa,
-            p.codigo_cliente,
-            data_previsao,
-            REGEXP_REPLACE(c.cnpj_cpf, '[^0-9]', '') as cnpj_cpf,
-            op.numero_op,
-            case  when pro.descricao is null then it.descricao else pro.descricao
-            end
-            as descricao_produto,
-            op.numero_CodProduto,	
-            case when op.quantidade is null then it.quantidade else op.quantidade
-            end as quantidade,
-            op.etapaid,
-            case when eop.descricao_etapa is null and rc.dtSugestao is not null then 
-            'Requisição de compra' else eop.descricao_etapa end as 'status_producao'
-			
-		FROM pedidos p
-		LEFT JOIN (SELECT * FROM etapas WHERE descricao_operacao = 'Venda de Produto') e ON e.codigo_etapa = p.etapa
-		INNER JOIN clientes c on c.codigo_cliente_omie = p.codigo_cliente
-		LEFT JOIN ordem_producao op on op.numero_pedido = p.numero_pedido
-		LEFT JOIN requisicoes_compra rc on rc.numero_pedido = p.numero_pedido
-		LEFT JOIN (SELECT * FROM etapas WHERE descricao_operacao = 'Ordem de Produção') eop ON eop.codigo_etapa = op.etapaid
-        LEFT JOIN produtos pro on pro.codigo_produto = op.numero_CodProduto
-        LEFT JOIN clientes cl on cl.codigo_cliente_omie = p.codigo_cliente
-        LEFT JOIN itens_pedido it on it.numero_pedido = p.numero_pedido
-		WHERE
-        (
-            eop.descricao_etapa IS NOT NULL
-            OR rc.dtSugestao IS NOT NULL
-        )
-        ORDER BY p.data_previsao DESC;
+                cl.razao_social,
+                p.numero_pedido,
+                p.etapa,
+                e.descricao_etapa,
+                p.codigo_cliente,
+                data_previsao,
+                REGEXP_REPLACE(c.cnpj_cpf, '[^0-9]', '') as cnpj_cpf,
+                op.numero_op,
+                case  when pro.descricao is null then it.descricao else pro.descricao
+                end
+                as descricao_produto,
+                op.numero_CodProduto,	
+                case when op.quantidade is null then it.quantidade else op.quantidade
+                end as quantidade,
+                op.etapaid,
+                case when eop.descricao_etapa is null and rc.dtSugestao is not null then 
+                'Requisição de compra' else eop.descricao_etapa end as 'status_producao',
+                it.codigo_produto
+                
+            FROM pedidos p
+            LEFT JOIN (SELECT * FROM etapas WHERE descricao_operacao = 'Venda de Produto') e ON e.codigo_etapa = p.etapa
+            INNER JOIN clientes c on c.codigo_cliente_omie = p.codigo_cliente
+            LEFT JOIN ordem_producao op on op.numero_pedido = p.numero_pedido
+            LEFT JOIN requisicoes_compra rc on rc.numero_pedido = p.numero_pedido
+            LEFT JOIN (SELECT * FROM etapas WHERE descricao_operacao = 'Ordem de Produção') eop ON eop.codigo_etapa = op.etapaid
+            LEFT JOIN produtos pro on pro.codigo_produto = op.numero_CodProduto
+            LEFT JOIN clientes cl on cl.codigo_cliente_omie = p.codigo_cliente
+            LEFT JOIN itens_pedido it on it.numero_pedido = p.numero_pedido
+            WHERE
+            (
+                eop.descricao_etapa IS NOT NULL
+                OR rc.dtSugestao IS NOT NULL
+            )
+            AND REGEXP_REPLACE(c.cnpj_cpf, '[^0-9]', '') = %s
+            group by p.numero_pedido,  it.codigo_produto
+            ORDER BY p.data_previsao DESC;
     """
 
     cursor.execute(sql)
