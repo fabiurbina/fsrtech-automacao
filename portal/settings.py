d900cade61ef0099d69928293834faeacd1b64e8
Fabio Urbina <fabiourbinasoares@gmail.com>
2026-08-17 23:19:01 -0300

    Remoção da dependência de MySQL da landing page

--- portal/settings.py
+++ portal/settings.py
@@ -91,15 +91,8 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DATABASE"),
-        'USER': os.getenv("USER"),
-        'PASSWORD': os.getenv("PASSWORD"),
-        'HOST': os.getenv("HOST"),
-        'PORT': os.getenv("DB_PORT", "3306"),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
