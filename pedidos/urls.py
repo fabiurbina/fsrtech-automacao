2cc45a2803f948cf51bf4b3448c76971558b4cea
Fabio Urbina <fabiourbinasoares@gmail.com>
2026-09-25 11:11:25 -0300

    Inclusão da estratégia

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home, criacao_de_sites
 
 
 urlpatterns = [
     path('', home, name='home'),
+    path('criacao-de-sites/', criacao_de_sites, name='criacao_de_sites'),
 ]
--- pedidos/views.py
+++ pedidos/views.py
@@ -1,6 +1,9 @@
-
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def criacao_de_sites(request):
+    return render(request, 'criacao_de_sites.html')
